25c7d952b34ca9485f1d0d7554e89d175b9fdc28
boctavian96 <boctavian96@yahoo.com>
2020-02-19 20:02:05 +0200

    Basic services.

---
+++ src/main/java/com/xml/serverlist/ServerListApp/database/ReadVerticle.java
@@ -0,0 +1,10 @@
+package com.xml.serverlist.ServerListApp.database;
+
+import io.vertx.core.AbstractVerticle;
+
+public class ReadVerticle extends AbstractVerticle {
+	@Override
+	public void start() {
+		// TODO
+	}
+}
---
+++ src/main/java/com/xml/serverlist/ServerListApp/database/UpdateVerticle.java
@@ -0,0 +1,10 @@
+package com.xml.serverlist.ServerListApp.database;
+
+import io.vertx.core.AbstractVerticle;
+
+public class UpdateVerticle extends AbstractVerticle {
+	@Override
+	public void start() {
+		// TODO
+	}
+}
