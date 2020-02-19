fd804c8ab0768e3bbceb0ba1bbe16c794c394548
boctavian96 <boctavian96@yahoo.com>
2020-02-19 16:38:20 +0200

    Hello world homepage.

---
+++ src/main/java/com/xml/serverlist/ServerListApp/homepage/BackendVerticle.java
@@ -0,0 +1,31 @@
+package com.xml.serverlist.ServerListApp.homepage;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import io.vertx.ext.web.Route;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
+
+public class BackendVerticle extends AbstractVerticle {
+	
+
+	@Override
+	public void start() {
+		Router router = Router.router(vertx);
+		Route messageRoute = router.get("/api/message");
+		messageRoute.handler(rc -> {
+			rc.response().end("Hello from Vert.x");
+		});
+		
+		router.get().handler(StaticHandler.create());
+		
+		vertx.createHttpServer() //
+		.requestHandler(router) //
+		.listen(8080);
+	}
+	
+	public static void main(String[] args) {
+		Vertx vertx = Vertx.vertx();
+		vertx.deployVerticle(new BackendVerticle());
+	}
+}
