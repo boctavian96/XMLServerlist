abfc613bd53c65a1bf03b969f7b719c08aa98430
boctavian96 <boctavian96@yahoo.com>
2020-02-21 11:54:58 +0200

    Modified the paths.

--- src/main/java/com/xml/serverlist/ServerListApp/homepage/XFormsService.java
+++ src/main/java/com/xml/serverlist/ServerListApp/homepage/XFormsService.java
@@ -4,7 +4,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
-@Path("/")
+@Path("/api")
 public class XFormsService {
 	@GET
 	@Path("/xforms")
--- src/main/java/com/xml/serverlist/ServerListApp/homepage/XmlRestService.java
+++ src/main/java/com/xml/serverlist/ServerListApp/homepage/XmlRestService.java
@@ -7,7 +7,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
-@Path("/")
+@Path("/xml")
 public class XmlRestService {
 
 	@GET
