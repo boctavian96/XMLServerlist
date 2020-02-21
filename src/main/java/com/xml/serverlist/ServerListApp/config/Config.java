7589ead0ef4567282aa7061e1f788a9e1469fb0e
boctavian96 <boctavian96@yahoo.com>
2020-02-21 23:52:09 +0200

    DOM4J.

---
+++ src/main/java/com/xml/serverlist/ServerListApp/config/Config.java
@@ -0,0 +1,5 @@
+package com.xml.serverlist.ServerListApp.config;
+
+public class Config {
+	public static final String DATABASE_FILE = "";
+}
--- src/main/java/com/xml/serverlist/ServerListApp/database/ReadVerticle.java
+++ src/main/java/com/xml/serverlist/ServerListApp/database/ReadVerticle.java
@@ -1,10 +1,57 @@
 package com.xml.serverlist.ServerListApp.database;
 
-import io.vertx.core.AbstractVerticle;
+import java.io.File;
 
-public class ReadVerticle extends AbstractVerticle {
-	@Override
-	public void start() {
-		// TODO
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+
+import org.dom4j.Document;
+import org.dom4j.io.DOMReader;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import com.xml.serverlist.ServerListApp.config.Config;
+
+public class ReadVerticle {
+
+	public String readSource(String xPathExpression) {
+		File xmlDatabase = new File(Config.DATABASE_FILE);
+		StringBuilder sb = new StringBuilder();
+		DOMReader reader = new DOMReader();
+
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder;
+		org.w3c.dom.Document doc;
+
+		Document document = null;
+		try {
+			dBuilder = dbFactory.newDocumentBuilder();
+			doc = dBuilder.parse(xmlDatabase);
+			document = reader.read(doc);
+
+			XPath xPath = XPathFactory.newInstance().newXPath();
+
+			NodeList nodeList = (NodeList) xPath.compile(xPathExpression).evaluate(doc, XPathConstants.NODESET);
+
+			for (int i = 0; i < nodeList.getLength(); i++) {
+				Node node = nodeList.item(i);
+
+				if (node.getNodeType() == Node.ELEMENT_NODE) {
+					Element element = (Element) node;
+
+					sb.append(element.getTextContent());
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return sb.toString();
 	}
+
 }
